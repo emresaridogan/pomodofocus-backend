32f7a2524c9cbcddeb4b6eb229bf61597d2f0a2f
Emresaridogan <emresaridogan@outlook.com>
2023-01-06 19:22:21 +0300

    results classes created

---
+++ Core/Utilities/Results/SuccessDataResult.cs
@@ -0,0 +1,36 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class SuccessDataResult<T> : DataResult<T>
+    {
+        private IDataResult<User> userToCheck;
+        private object successfulLogin;
+
+        public SuccessDataResult(T data, string message) : base(data,true,message)
+        {
+
+        }
+
+        public SuccessDataResult(T data) : base(data,true)
+        {
+
+        }
+
+        public SuccessDataResult(string message) : base(default,true,message)
+        {
+
+        }
+
+        public SuccessDataResult() : base(default,true)
+        {
+
+        }
+
+    }
+}
