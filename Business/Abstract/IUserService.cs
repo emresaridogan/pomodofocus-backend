7b626461fbe6f743b1c1575238c646cce8afea92
Emresaridogan <emresaridogan@outlook.com>
2023-01-06 19:21:07 +0300

    user service interface created

---
+++ Business/Abstract/IUserService.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IUserService
+    {
+        List<OperationClaim> GetOperationClaims(User user);
+        List<OperationClaim> GetOperationClaims();
+
+        User GetUserByEmail(string email);
+        IDataResult<List<User>> GetUsers();
+
+        IResult AddUser(User user);
+        IResult DeleteUser(User user);
+        IResult UpdateUser(User user);
+    }
+}
