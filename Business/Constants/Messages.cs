06f1a44225c07ae07377646b80e12722fc1a5305
Emresaridogan <emresaridogan@outlook.com>
2023-01-06 19:19:45 +0300

    messages constants created

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string UserAdded = "Kullanıcı başarıyla eklendi";
+        public static string UserDeleted = "Kullanıcı başarıyla silindi";
+        public static string UserUpdated = "Kullanıcı başarıyla güncellendi";
+
+        public static string UserNotFound = " Kullanıcı bulunamadı";
+        public static string PasswordError = " Şifre yanlış";
+        public static string SuccessfullLogin = " Giriş başarılı";
+
+        public static string UserAlreadyExists = "Bu email adresine ait bir kullanıcı mevcut";
+        public static string UserRegistered = "Kullanıcı kaydedildi";
+        public static string AccessTokenCreated = "Access token oluşturuldu";
+    }
+}
