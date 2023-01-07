6302b230a855e34839c0711b0a7bea066be3a175
Emresaridogan <emresaridogan@outlook.com>
2023-01-07 15:27:59 +0300

    user dao interface created

---
+++ DataAccess/Abstract/IUserDao.cs
@@ -0,0 +1,17 @@
+﻿using Core.DataAccess;
+using Core.Entities.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IUserDao : IEntityRepository<User>
+    {
+        List<OperationClaim> GetOperationClaims(User user);
+        List<OperationClaim> GetOperationClaims();
+    }
+}
